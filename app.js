e8606b741d9ea16a644c8ecd9553a1fcc1ab751f
Marcus Vinicius <marcusvinicius2097@gmail.com>
2022-12-31 20:51:30 -0300

    update route and CRUD database

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+require('dotenv/config');
 const bodyParser = require("body-parser");
 const { render } = require("ejs");
 const express = require("express");
 const app = express();
-const date = require(__dirname+"/date.js")
 const mongoose = require("mongoose")
+const _ = require('lodash')
+
+
+//MgvAuaeZmUbqqOBY
 
 mongoose.set("strictQuery", false);
 
-const day = date.getDate();
-let list = []
 let statusAdd = "false";
 
 app.set("view engine", "ejs"); //para o EJS Funcionar
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://marcusvscosta:Le2fRkOPErFcchyP@cluster0.wcibe94.mongodb.net", { useNewUrlParser: true})
-let listItem = []
+mongoose.connect("mongodb+srv://" + process.env.USER_NAME + ":" + process.env.PASSWORD + "@cluster0.wcibe94.mongodb.net/todolistDB", { useNewUrlParser: true})
 
 const itemSchema = new mongoose.Schema(
     {
@@ -30,28 +30,29 @@ const itemSchema = new mongoose.Schema(
 
 const Item = mongoose.model("Item", itemSchema)
 
-function newItem(item){
-    const newItem = new Item({
-        name: item
-    })
+const listSchema = new mongoose.Schema(
+    {
+        name:String,
+        items: [itemSchema]
+    }
+)
 
-    newItem.save();
-}
+const List = mongoose.model("List", listSchema)
+
+const item1 = new Item({
+    name: "Exemplo Item"
+})
+
+const defultItems = [item1]
 
 
 app.get("/", function(req, res){
-    listItem = []
     Item.find(function(err, rItens){
         if(err){
             console.log(err);
         }else{
-            rItens.forEach(function(rItem){
-                listItem.push(rItem.name)
-            })
-            list = listItem;
-            console.log(listItem);
-            res.render("list", {listTitle: "List", dateAtual: day, itens: listItem, status: statusAdd});
-            statusAdd = "false";
+           res.render("list", {listTitle: "Today", itens: rItens, status: statusAdd});
+           statusAdd = "false";
         }
     })
 })
@@ -59,26 +60,65 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
 
     const item = req.body.newItem
+    const nameList = req.body.list
 
     if(item === '' || item === null){
         //workList.push(item)
-        statusAdd = "true";
-        res.redirect("/")
+        if(nameList === "Today"){
+            statusAdd = "true";
+            res.redirect("/")
+        }else{
+            statusAdd = "true";
+            res.redirect("/" + nameList)
+        }   
     }else{
         //listItem.push(item);
-        newItem(item);
-        console.log(listItem);
-        res.redirect("/")
-        console.log(req.body.list);
+        const newItem = new Item({
+            name: item
+        })
+        
+        if(nameList === "Today"){
+            newItem.save();
+            res.redirect("/")
+        }else{
+            List.findOne({name: nameList}, function(err, results){
+                results.items.push(newItem);
+                results.save();
+                res.redirect("/" + nameList);
+            })
+        }
         statusAdd = "false";
-
     }
-    console.log(req.body.remove)
    
 });
 
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work", dateAtual: day, itens: workList});
+app.get('/:customListName', function(req, res) {
+    const customListName = _.capitalize(req.params.customListName);
+
+    if(customListName === "Favicon.ico"){
+
+    }else{
+         List.findOne({name: customListName}, function(err, results){
+
+            if(!err){
+                if(!results){
+                    //creat new list
+                    const list = new List({
+                        name: customListName,
+                        items: defultItems
+                    })
+                    list.save();
+                    console.log("Não Existe a lista");
+                    res.redirect("/" + customListName)
+                }else{
+                    console.log("A lista existe");
+                    res.render("list", {listTitle: customListName, itens: results.items, status: statusAdd});
+                    statusAdd = "false";
+                }
+            }
+            //console.log(results.items);
+        })
+    }   
 });
 
 app.get("/about", function(req, res){
@@ -86,21 +126,27 @@ app.get("/about", function(req, res){
 })
 
 app.post("/remove", function(req, res){
-    const number = req.body.remove;
+    const idRemove = req.body.remove;
+    const titleName = req.body.titleName;
 
-    Item.deleteOne({name: list[number]}, function(err){
-        if(err){
-            console.log(err);
-        }else{
-        console.log("Sucesse delete Item");
-        }
-    });
+    if(titleName === "Today"){
+        Item.deleteOne({_id: idRemove}, function(err){
+            if(err){
+                console.log(err);
+            }else{
+            console.log("Sucesse delete Item");
+            }
+        });    
+        res.redirect("/")
+    }else{
+        List.findOneAndUpdate({name: titleName}, {$pull: {items:{_id: idRemove}}}, function(err){
+            if(!err){
+                res.redirect("/" + titleName)
+            }
+        })
 
-    console.log(list[number]);
+    }
   
-
-     
-    res.redirect("/")
 })
 
 app.listen(3000, function(){
